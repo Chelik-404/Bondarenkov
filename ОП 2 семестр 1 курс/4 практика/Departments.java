dd3ab15e43d358a35b9a8a296113f01389fae6d6
Chelik-404 <workchelik@gmail.com>
2022-06-05 22:49:56 +0300

    практика 4

---
+++ ОП 2 семестр 1 курс/4 практика/Departments.java
@@ -0,0 +1,29 @@
+class Departments {
+
+    private String dep;
+
+
+
+
+    void setDep(String dep){
+        this.dep=dep
+        ;
+    }
+    String getDep(){
+        return this.dep;
+    }
+
+
+
+    public String toString(){
+        return "Кафедра " + this.dep + ".";
+    }
+
+
+
+
+
+
+
+
+}
